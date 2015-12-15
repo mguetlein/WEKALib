7554ac13de2b0bd686bed817a47ed15dcaed5806
Martin Gütlein <guetlein@posteo.de>
2015-12-15 19:34:48 +0100

    moved cfp stuff to wekalib

---
+++ src/main/java/org/mg/wekalib/appdomain/AppDomainModel.java
@@ -0,0 +1,11 @@
+package org.mg.wekalib.appdomain;
+
+import weka.core.Instance;
+import weka.core.Instances;
+
+public interface AppDomainModel
+{
+	public void build(Instances trainingData);
+
+	public boolean isInsideAppdomain(Instance testInstance);
+}
